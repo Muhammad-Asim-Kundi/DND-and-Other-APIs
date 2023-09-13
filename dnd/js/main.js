a8ea9281bc10705bcf2481a390f7d3a90d32849b
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2023-09-13 23:20:52 +0500

    started work on Dnd API in class-28 Materials

---
+++ dnd/js/main.js
@@ -0,0 +1,21 @@
+//Example fetch using DnD5eAPI - place subclasses in ul
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+       console.log(data)
+       document.querySelector('h3').innerText = data.choice[0].material
+       document.querySelector('h4').innerText = data.choice.subclasses[0]
+
+
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
