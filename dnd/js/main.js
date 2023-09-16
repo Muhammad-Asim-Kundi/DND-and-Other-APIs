e3d413fafcf2786968b44182d9fda233bddf6b7d
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2023-09-16 23:46:45 +0500

    Added another API of OPEN-LIBRARY BOOK API along with dnd for better understanding of APIs

--- dnd/js/main.js
+++ dnd/js/main.js
@@ -31,27 +31,49 @@
 
 // Just practiced the code again 
 
+// document.querySelector('button').addEventListener('click', getFetch)
+
+// function getFetch(){
+//   const choice = document.querySelector('input').value
+//   const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+//   fetch(url)
+//   .then(res => res.json())
+//   .then(data => {
+//     data.subclasses.forEach(obj =>{
+//       console.log(obj.name)
+//       //creat li
+//       const li = document.createElement('li')
+//       //add text to li
+//       li.textContent = obj.name
+//       //append the li to the ul 
+//       document.querySelector('ul').appendChild(li)
+
+//     })
+//     .catch(err => {
+//       console.log(`error ${err}`)
+//     })
+//   })
+// }
+
+// BOOK API
+
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
-  fetch(url)
-  .then(res = res.json())
-  .then(data => {
-    console.log(data.subclasses)
-    data.subclasses.forEach(obj =>{
-      console.log(obj.name)
-      //creat li
-      const li = document.createElement('li')
-      //add text to li
-      li.textContent = obj.name
-      //append the li to the ul 
-      document.querySelector('ul').appendChild(li)
+  // const choice = document.querySelector('input').value
+  const url = `https://openlibrary.org/works/OL45804W.json`
 
-    })
-    .catch(err => {
-      console.log(`error ${err}`)
-    })
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    document.querySelector('h2').innerText = data.title
+    document.querySelector('h3').innerText = data.description
+    
+
   })
+  .catch(err => {
+      console.log(`error ${err}`)
+  })
+  
 }
