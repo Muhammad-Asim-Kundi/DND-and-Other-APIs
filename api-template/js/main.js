b6f2ed66e0475464164b8549dc31fa6bd855885d
Asim Kundi <muhammadasimkundi@gmail.com>
2024-07-21 21:58:55 +0500

    Update main.js

--- api-template/js/main.js
+++ api-template/js/main.js
@@ -5,7 +5,6 @@ function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
   const url = `https://api.nasa.gov/planetary/apod?api_key=iJy0Nd4wZZzfW2HFgBkNV4DDUsZ6PxL3RqQRgFfI&date=${choice}`
-  console.log(url)
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
